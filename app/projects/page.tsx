3ecfc21a8ebaa8f29e9a4a9401c1143a1db03d91
goodnesslu <goodnesslu.dev@gmail.com>
2024-02-26 12:24:41 +0300

    First-commit

---
+++ app/projects/page.tsx
@@ -0,0 +1,37 @@
+import projectsData from '@/data/projectsData'
+import Card from '@/components/Card'
+import { genPageMetadata } from 'app/seo'
+
+export const metadata = genPageMetadata({ title: 'Projects' })
+
+export default function Projects() {
+  return (
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            I know - I try a lot of stuff
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Quantity builds quality. Excellence is built through effective repition. Take a look at
+            some of my projects - FYI, they are not all great.
+            {/* Showcase your projects with a hero image (16 x 9) */}
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
