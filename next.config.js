f03ea40a3242dac76525b72281db40f99392e564
goodnesslu <goodnesslu.dev@gmail.com>
2024-02-26 13:27:36 +0300

    changes5

--- next.config.js
+++ next.config.js
@@ -59,8 +59,16 @@ const securityHeaders = [
  **/
 module.exports = () => {
   const nextConfig = {
-    // Remove `output: 'export'` to fix next export error on GitHub Pages
-    target: 'experimental-serverless-trace',
+    output: 'export',
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
   }
 
   const plugins = [withContentlayer, withBundleAnalyzer]
